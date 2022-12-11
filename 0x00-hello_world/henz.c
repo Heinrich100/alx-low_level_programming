e44b84d6616e180e71ab667d98b16ac02cfb4e1d
Ezedigwe Henry <henryezedigwe@gmail.com>
2022-12-11 20:33:12 +0000

    assembly code in intel syntax

---
+++ 0x00-hello_world/henz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints assembly code in intel syntax
+ *
+ * Description: program compiles but dosen't assembly 
+ *Therefor printing assembly codes
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	return (0);
+}
