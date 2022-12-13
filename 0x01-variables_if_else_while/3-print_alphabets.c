314b3e3d7b084418f0eec975bd5b7975932e5bee
Ezedigwe Henry <henryezedigwe@gmail.com>
2022-12-13 01:00:07 +0000

    prints lowercase & uppercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints both lowercase and uppercase alphabets
+ *
+ * Description: programs prints using putchar function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar (ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar (ch);
+	}
+	putchar ('\n');
+	return (0);
+}
