f86a3b3aa0013340e65f09cce1f0396c89fa605d
Ezedigwe Henry <henryezedigwe@gmail.com>
2022-12-13 16:45:14 +0000

    print single numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program to print nummbers
+ *
+ * Description: program will print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
