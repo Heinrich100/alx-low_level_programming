b1d92a03717ad0cbeaf6dd60f5a215888eac8f90
Ezedigwe Henry <henryezedigwe@gmail.com>
2022-12-18 12:13:51 +0000

    fixed some bug

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,6 @@ void jack_bauer(void)
 {
 	int a, b, c, d;
 
-<<<<<<< HEAD
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
@@ -28,27 +27,8 @@ void jack_bauer(void)
 						_putchar(d + '0');
 						_putchar('\n');
 					}
-=======
-	for (a = 0; b <= 9; b++)
-	{
-		if ((a <= 1 && b <= 9) || (a <= 2 && b <=3))
-		{
-			for (c = 0; c <= 5; c++)
-			{
-				for (d = 0; d <= 9; d++)
-				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(58);
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
->>>>>>> 6b3b000c66ea5dafa5c3d689b0beaba0266c6da3
 				}
 			}
 		}
 	}
-<<<<<<< HEAD
-=======
 }
->>>>>>> 6b3b000c66ea5dafa5c3d689b0beaba0266c6da3
