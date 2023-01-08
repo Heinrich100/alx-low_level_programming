43fbdd5b65e6785603f394d1b49fc765231eb8a9
Ezedigwe Henry <henryezedigwe@gmail.com>
2023-01-08 14:34:27 +0000

    concatenete using strancat concept

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strncat - Concatenates two strings
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The limit of the concatenation
+  *
+  * Return: A pointer to the resulting string dest
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[len + i] = src[i];
+	dest[len + i] = '\0';
+
+	return (dest);
+}
